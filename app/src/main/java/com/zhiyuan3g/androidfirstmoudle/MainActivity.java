7a5464d23aa4d58adef2e39e1416afd986cb8578
zcy <zcychinese@yeah.net>
2017-08-22 15:25:20 +0800

    确定外层框架，添加了ToolBar和侧滑

--- app/src/main/java/com/zhiyuan3g/androidfirstmoudle/MainActivity.java
+++ app/src/main/java/com/zhiyuan3g/androidfirstmoudle/MainActivity.java
@@ -1,13 +1,40 @@
 package com.zhiyuan3g.androidfirstmoudle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolBar)
+    Toolbar toolBar;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        //初始化ToolBar
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        //设置ToolBar生效
+        setSupportActionBar(toolBar);
+        //通过获取ActionBar，设置导航键，隐藏标题
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.home);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
     }
 }
