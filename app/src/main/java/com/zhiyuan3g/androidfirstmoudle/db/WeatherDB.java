d7ca2fa444a932537bcf35d459a33008812b2d85
zcy <zcychinese@yeah.net>
2017-08-28 17:19:15 +0800

    剩下最后的定时更新未完成。

---
+++ app/src/main/java/com/zhiyuan3g/androidfirstmoudle/db/WeatherDB.java
@@ -0,0 +1,226 @@
+package com.zhiyuan3g.androidfirstmoudle.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by kkkkk on 2017/8/28.
+ */
+
+public class WeatherDB extends DataSupport {
+    private int id;
+    private String title;
+    private String wendu;
+    private String state;
+    private String oneOne;
+    private String oneTwo;
+    private String oneThree;
+    private String oneFour;
+    private String twoOne;
+    private String twoTwo;
+    private String twoThree;
+    private String twoFour;
+    private String threeOne;
+    private String threeTwo;
+    private String threeThree;
+    private String threeFour;
+    private String air;
+    private String comf;
+    private String cw;
+    private String drsg;
+    private String flu;
+    private String sport;
+    private String trav;
+    private String uv;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWendu() {
+        return wendu;
+    }
+
+    public void setWendu(String wendu) {
+        this.wendu = wendu;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getOneOne() {
+        return oneOne;
+    }
+
+    public void setOneOne(String oneOne) {
+        this.oneOne = oneOne;
+    }
+
+    public String getOneTwo() {
+        return oneTwo;
+    }
+
+    public void setOneTwo(String oneTwo) {
+        this.oneTwo = oneTwo;
+    }
+
+    public String getOneThree() {
+        return oneThree;
+    }
+
+    public void setOneThree(String oneThree) {
+        this.oneThree = oneThree;
+    }
+
+    public String getOneFour() {
+        return oneFour;
+    }
+
+    public void setOneFour(String oneFour) {
+        this.oneFour = oneFour;
+    }
+
+    public String getTwoOne() {
+        return twoOne;
+    }
+
+    public void setTwoOne(String twoOne) {
+        this.twoOne = twoOne;
+    }
+
+    public String getTwoTwo() {
+        return twoTwo;
+    }
+
+    public void setTwoTwo(String twoTwo) {
+        this.twoTwo = twoTwo;
+    }
+
+    public String getTwoThree() {
+        return twoThree;
+    }
+
+    public void setTwoThree(String twoThree) {
+        this.twoThree = twoThree;
+    }
+
+    public String getTwoFour() {
+        return twoFour;
+    }
+
+    public void setTwoFour(String twoFour) {
+        this.twoFour = twoFour;
+    }
+
+    public String getThreeOne() {
+        return threeOne;
+    }
+
+    public void setThreeOne(String threeOne) {
+        this.threeOne = threeOne;
+    }
+
+    public String getThreeTwo() {
+        return threeTwo;
+    }
+
+    public void setThreeTwo(String threeTwo) {
+        this.threeTwo = threeTwo;
+    }
+
+    public String getThreeThree() {
+        return threeThree;
+    }
+
+    public void setThreeThree(String threeThree) {
+        this.threeThree = threeThree;
+    }
+
+    public String getThreeFour() {
+        return threeFour;
+    }
+
+    public void setThreeFour(String threeFour) {
+        this.threeFour = threeFour;
+    }
+
+    public String getAir() {
+        return air;
+    }
+
+    public void setAir(String air) {
+        this.air = air;
+    }
+
+    public String getComf() {
+        return comf;
+    }
+
+    public void setComf(String comf) {
+        this.comf = comf;
+    }
+
+    public String getCw() {
+        return cw;
+    }
+
+    public void setCw(String cw) {
+        this.cw = cw;
+    }
+
+    public String getDrsg() {
+        return drsg;
+    }
+
+    public void setDrsg(String drsg) {
+        this.drsg = drsg;
+    }
+
+    public String getFlu() {
+        return flu;
+    }
+
+    public void setFlu(String flu) {
+        this.flu = flu;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getTrav() {
+        return trav;
+    }
+
+    public void setTrav(String trav) {
+        this.trav = trav;
+    }
+
+    public String getUv() {
+        return uv;
+    }
+
+    public void setUv(String uv) {
+        this.uv = uv;
+    }
+}
